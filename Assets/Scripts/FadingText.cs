f3051281c2cf8990fba58a877c3844fb801ad103
WilliamHienTang <williamhientang@gmail.com>
2018-09-12 17:52:46 -0700

    added tap screen

---
+++ Assets/Scripts/FadingText.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadingText : MonoBehaviour {
+    Text text;
+
+	// Use this for initialization
+	void Start () {
+        text = GetComponent<Text>();
+        //Debug.Log(text.text);
+        StartFading();
+	}
+	
+    IEnumerator Fade()
+    {
+        while (true)
+        {
+            switch (text.color.a.ToString())
+            {
+                case "0":
+                    StartCoroutine(FadeTextToFullAlpha(1.0f, text));
+                    yield return new WaitForSeconds(0.5f);
+                    break;
+                case "1":
+                    StartCoroutine(FadeTextToZeroAlpha(1.0f, text));
+                    yield return new WaitForSeconds(0.5f);
+                    break;
+            }
+        }
+    }
+
+    public IEnumerator FadeTextToFullAlpha(float time, Text text)
+    {
+        text.color = new Color(text.color.r, text.color.g, text.color.b, 0);
+        while (text.color.a < 1.0f)
+        {
+            text.color = new Color(text.color.r, text.color.g, text.color.b, text.color.a + (Time.deltaTime / time));
+            yield return null;
+        }
+    }
+
+    public IEnumerator FadeTextToZeroAlpha(float time, Text text)
+    {
+        text.color = new Color(text.color.r, text.color.g, text.color.b, 1);
+        while (text.color.a > 0.0f)
+        {
+            text.color = new Color(text.color.r, text.color.g, text.color.b, text.color.a - (Time.deltaTime / time));
+            yield return null;
+        }
+    }
+
+    void StartFading()
+    {
+        StopCoroutine("Fade");
+        StartCoroutine("Fade");
+    }
+
+    void StopFading()
+    {
+        StopCoroutine("Fade");
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/ScreenFade.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class ScreenFade : MonoBehaviour {
+
+    public Image background;
+    public string loadLevel;
+    int touches;
+
+    IEnumerator Start()
+    {
+        enabled = false;
+        background.canvasRenderer.SetAlpha(0.0f);
+        FadeInImage();
+        yield return new WaitForSeconds(2.5f);
+        touches = 0;
+        enabled = true;
+    }
+
+    void FadeInImage()
+    {
+        background.CrossFadeAlpha(1.0f, 1.5f, false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        touches = Input.touchCount;
+        if (touches > 0)
+        {
+            SceneManager.LoadScene(loadLevel);
+        }
+	}
+}
