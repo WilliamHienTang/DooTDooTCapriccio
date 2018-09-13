460b7c79c6322a175bee99fd7e25d706b9f27453
WilliamHienTang <williamhientang@gmail.com>
2018-09-13 10:02:18 -0700

    fixed title screen fade

--- Assets/Scripts/ScreenFade.cs
+++ Assets/Scripts/ScreenFade.cs
@@ -8,6 +8,7 @@ using UnityEngine.SceneManagement;
 public class ScreenFade : MonoBehaviour {
 
     public Image background;
+    public Text text;
     public string loadLevel;
     int touches;
 
@@ -15,7 +16,10 @@ public class ScreenFade : MonoBehaviour {
     {
         enabled = false;
         background.canvasRenderer.SetAlpha(0.0f);
+        text.canvasRenderer.SetAlpha(0.0f);
         FadeInImage();
+        yield return new WaitForSeconds(0.75f);
+        FadeInText();
         yield return new WaitForSeconds(0.75f);
         touches = 0;
         enabled = true;
@@ -26,6 +30,11 @@ public class ScreenFade : MonoBehaviour {
         background.CrossFadeAlpha(1.0f, 0.75f, false);
     }
 	
+    void FadeInText()
+    {
+        text.CrossFadeAlpha(1.0f, 0.75f, false);
+    }
+
 	// Update is called once per frame
 	void Update () {
         touches = Input.touchCount;
