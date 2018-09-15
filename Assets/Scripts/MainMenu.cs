a796bda3c70dc050c68365ba5f44aac27de18bcb
WilliamHienTang <williamhientang@gmail.com>
2018-09-14 17:52:12 -0700

    gonna need some better background gifs

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,14 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour {
 
-    public void PlayGame()
+    public void LoadSongScreen()
     {
         SceneManager.LoadScene("ScoreScreen");
     }
 
-    public void QuitGame()
+    public void LoadTitleScreen()
     {
-        Application.Quit();
+        SceneManager.LoadScene("TitleScreen");
     }
 
 	// Use this for initialization
