d6479d3d14269344b4f75bcaa3d32a7bef8631d2
WilliamHienTang <williamhientang@gmail.com>
2018-09-14 17:29:45 -0700

    added scorescreen

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -7,7 +7,7 @@ public class MainMenu : MonoBehaviour {
 
     public void PlayGame()
     {
-        SceneManager.LoadScene("MainMenu");
+        SceneManager.LoadScene("ScoreScreen");
     }
 
     public void QuitGame()
