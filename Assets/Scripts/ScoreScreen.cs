e08703d70d9e6c9faa3821df7743a51f0c7a3f9b
WilliamHienTang <williamhientang@gmail.com>
2018-09-17 22:16:47 -0700

    work on options next

--- Assets/Scripts/ScoreScreen.cs
+++ Assets/Scripts/ScoreScreen.cs
@@ -7,13 +7,24 @@ public class ScoreScreen : MonoBehaviour {
 
     public void LoadMainMenu()
     {
+        StopAudio();
         SceneManager.LoadScene("MainMenu");
+    }
+
+    public void ButtonAudio()
+    {
+        FindObjectOfType<AudioManager>().Play("bubble2");
+    }
+
+    void StopAudio()
+    {
+        FindObjectOfType<AudioManager>().Stop("Kaiheitai");
     }
 
 	// Use this for initialization
 	void Start () {
-		
-	}
+        FindObjectOfType<AudioManager>().Play("Kaiheitai");
+    }
 	
 	// Update is called once per frame
 	void Update () {
