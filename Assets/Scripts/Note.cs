47e5425f4358603851845610675d042e4a175c2f
WilliamHienTang <williamhientang@gmail.com>
2018-10-02 21:47:54 -0700

    made noteplatform flat

---
+++ Assets/Scripts/Note.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Note : MonoBehaviour {
+
+    Rigidbody RB;
+
+	// Use this for initialization
+	void Start () {
+        RB = GetComponent<Rigidbody>();
+        RB.velocity = new Vector3(0, 0, -1);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
