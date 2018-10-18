4d93ce42ed07a4e6cfb295f8c3a8964a68e5d59d
WilliamHienTang <williamhientang@gmail.com>
2018-10-17 20:44:12 -0700

    buh

--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -4,44 +4,94 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour {
 
-    public string songName;
-    int[] noteLanes = {1, 5, 5, 4, 3, 2, 5, 2, 5, 4, 5, 2, 3, 5, 3, 5, 5};
-    int noteIndex = 0;
-    bool timerReset = true;
-    float xPosition;
     public Transform noteObject;
     public Transform holdNoteObject;
+    float velocityOffset;
+    readonly float songDelay = 3.0f;
+    readonly float spawnDistance = 20.0f;
+    bool flag = true;
 
-    IEnumerator SpawnNote()
+    // Use this for initialization
+    IEnumerator Start()
     {
-        yield return new WaitForSeconds(1.0f);
+        enabled = false;
+        PlayerPrefs.SetInt("Score", 0);
+        PlayerPrefs.SetInt("Combo", 0);
 
-        if(noteLanes[noteIndex] == 1)
+        FindObjectOfType<AudioManager>().Play(PlayerPrefs.GetString("SelectedSong"));
+        velocityOffset = spawnDistance / PlayerPrefs.GetFloat("NoteSpeed");
+        yield return new WaitForSeconds(songDelay - velocityOffset);
+        enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (flag)
         {
-            xPosition = -1.02f;
+            InstantiateNote(4);
+            //InstantiateHoldNote(2, 1.0f);
         }
-        else if (noteLanes[noteIndex] == 2)
+        flag = false;
+    }
+
+    void InstantiateNote(int laneIndex)
+    {
+        float xPosition;
+        switch (laneIndex)
         {
-            xPosition = -0.51f;
-        }
-        else if (noteLanes[noteIndex] == 3)
-        {
-            xPosition = 0f;
-        }
-        else if (noteLanes[noteIndex] == 4)
-        {
-            xPosition = 0.51f;
-        }
-        else if (noteLanes[noteIndex] == 5)
-        {
-            xPosition = 1.02f;
+            case 1:
+                xPosition = -1.02f;
+                break;
+            case 2:
+                xPosition = -0.51f;
+                break;
+            case 3:
+                xPosition = 0f;
+                break;
+            case 4:
+                xPosition = 0.51f;
+                break;
+            case 5:
+                xPosition = 1.02f;
+                break;
+            default:
+                return;
         }
 
-        noteIndex++;
-        timerReset = true;
+        Instantiate(noteObject, new Vector3(xPosition, 0.0f, spawnDistance), noteObject.rotation);
+    }
 
-        //Instantiate(noteObject, new Vector3(xPosition, 0.0f, 10.0f), noteObject.rotation);
-        Instantiate(holdNoteObject, new Vector3(xPosition, 0.0f, 10.0f), holdNoteObject.rotation);
+    void InstantiateHoldNote(int laneIndex, float length)
+    {
+        float xPosition;
+        switch (laneIndex)
+        {
+            case 1:
+                xPosition = -1.02f;
+                break;
+            case 2:
+                xPosition = -0.51f;
+                break;
+            case 3:
+                xPosition = 0f;
+                break;
+            case 4:
+                xPosition = 0.51f;
+                break;
+            case 5:
+                xPosition = 1.02f;
+                break;
+            default:
+                return;
+        }
+
+        Transform holdNote = Instantiate(holdNoteObject, new Vector3(xPosition, 0.0f, spawnDistance), holdNoteObject.rotation);
+        //GameObject holdLane = holdNote.transform.Find("HoldLane").gameObject;
+        //holdNote.transform.Find("HoldLane").localPosition = new Vector3(holdLane.transform.localPosition.x, holdLane.transform.localPosition.y, length / 2.0f);
+        //holdNote.transform.Find("HoldLane").localScale = new Vector3(holdLane.transform.localScale.x, holdLane.transform.localScale.y, length);
+        //GameObject tailNote = holdNote.transform.Find("Tail").gameObject;
+        //holdNote.transform.Find("Tail").localPosition = new Vector3(tailNote.transform.localPosition.x, tailNote.transform.localPosition.y, length);
     }
 
     public void IncreaseScore(int points)
@@ -58,22 +108,4 @@ public class GameManager : MonoBehaviour {
     {
         PlayerPrefs.SetInt("Combo", 0);
     }
-
-	// Use this for initialization
-	IEnumerator Start () {
-        PlayerPrefs.SetInt("Score", 0);
-        PlayerPrefs.SetInt("Combo", 0);
-
-        yield return new WaitForSeconds(1.0f);
-        FindObjectOfType<AudioManager>().Play(PlayerPrefs.GetString("SelectedSong"));
-    }
-	
-	// Update is called once per frame
-	void Update () {
-        if (timerReset && noteIndex < noteLanes.Length)
-        {
-            timerReset = false;
-            StartCoroutine(SpawnNote());
-        }
-	}
 }
---
+++ Assets/Scripts/Game/NoteSpawn.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteSpawn : MonoBehaviour {
+    public float spawnTime;
+    public float tailSpawnTime;
+    public int laneIndex;
+
+    public float length;
+
+    // Use this for initialization
+    void Awake()
+    {
+        if (tailSpawnTime > 0)
+        {
+            length = PlayerPrefs.GetFloat("velocty") * (tailSpawnTime - spawnTime);
+        }
+        else
+        {
+            length = 0;
+        }
+    }
+}
